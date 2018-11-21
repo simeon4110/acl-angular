992b80a3cfbc86becab2b1d54ed16fab2e0de53b
simeon4110 <josh@joshharkema.com>
2018-11-20 17:29:20 -0700

    #1 - Home page styling mocked up, basic elements of style and typography are in. Much more to go.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+/**
+ * Manages root functions for the app.
+ *
+ * @author Josh Hakema
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'acl';
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available. Load new version?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,46 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavComponent } from './core/nav/nav.component';
-import { FooterComponent } from './core/footer/footer.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NavComponent} from './core/nav/nav.component';
+import {FooterComponent} from './core/footer/footer.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
-import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {BrowseComponent} from './modules/browse/browse.component';
+import {SearchComponent} from './modules/search/search.component';
+import {HomeComponent} from './modules/home/home.component';
+import {AboutComponent} from './modules/about/about.component';
+import {ToolsComponent} from './modules/tools/tools.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'browse', component: BrowseComponent},
+  {path: 'search', component: SearchComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'tools', component: ToolsComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    BrowseComponent,
+    SearchComponent,
+    HomeComponent,
+    AboutComponent,
+    ToolsComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -23,9 +48,13 @@ import {FlexLayoutModule} from "@angular/flex-layout";
     MatButtonModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
