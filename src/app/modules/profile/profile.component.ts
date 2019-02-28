e7f31da54a12fd001aa94babe603628dc540c8b0
simeon4110 <josh@joshharkema.com>
2019-02-28 15:08:32 -0700

    Fixed permissions issue.
    Signed-off-by: simeon4110 <josh@joshharkema.com>

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -8,7 +8,7 @@ import {AuthService} from '../../core/auth/auth.service';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor(private auth: AuthService) {
+  constructor(public auth: AuthService) {
   }
 
   ngOnInit() {
